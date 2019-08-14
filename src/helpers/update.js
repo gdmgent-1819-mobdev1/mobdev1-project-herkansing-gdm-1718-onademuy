c5e673493c76c33825f12858f9124aea546264ba
Ona Demuytere <onademuy@student.arteveldehs.be>
2019-08-14 23:40:15 +0200

    last commit

--- src/helpers/update.js
+++
@@ -1,8 +0,0 @@
-const update = (content) => {
-  // Get the app container
-  const app = document.getElementById('app');
-  // Replace the content with the new content
-  app.innerHTML = content;
-};
-
-export default update;
